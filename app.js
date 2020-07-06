d9a60923455094c70a4c746fb01d5b4b6c59a50c
Julia Shybunka <julishybunka@gmail.com>
2020-07-06 16:45:21 +0300

    Add mongoose. Create postDB.

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 var _ = require('lodash');
 const { forEach } = require('lodash');
+const mongoose = require('mongoose');
 
 
 const app = express();
 
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 
 const posts = [];
 
@@ -33,12 +45,12 @@ app.post('/addPost', function(req, res) {
     let postTitle = req.body.postTitle;
     let postBody = req.body.postBody;
 
-    let post = {
+    const post = new Post({
         title: postTitle,
         content: postBody
-    }
+    });
 
-    posts.push(post);
+    post.save();
     res.redirect('/');
 })
 
